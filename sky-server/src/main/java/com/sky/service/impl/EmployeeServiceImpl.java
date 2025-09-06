ba632ab02c888318a3173c8991d6014aa67aa154
liuyujian <2778706656@qq.com>
2025-09-06 15:57:54 +0800

    新增员工业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,10 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,9 +12,12 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -39,7 +45,7 @@ public class EmployeeServiceImpl implements EmployeeService {
         }
 
         //密码比对
-        // TODO 后期需要进行md5加密，然后再进行比对
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
         if (!password.equals(employee.getPassword())) {
             //密码错误
             throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
@@ -53,5 +59,40 @@ public class EmployeeServiceImpl implements EmployeeService {
         //3、返回实体对象
         return employee;
     }
+
+    /**
+     * 新增员工
+     * @param employeeDTO
+     */
+    public void save(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+        System.out.println("当前的线程id ： " + Thread.currentThread().getId());
+        //将DTO数据传输对象装换为java对象进行操作，此处使用BeanUtils.copyProperties对前端json传输过来的属性进行复制到我们的employee对象
+        BeanUtils.copyProperties(employeeDTO,employee);
+
+        //此处设置员工的默认密码，使用常量类进行对默认密码的设置，避免了硬编码,需要对这个密码进行md5加密处理
+        employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
+
+        //设置员工账号的状态，默认是正常状态，1表示正常，0表示锁定
+        employee.setStatus(StatusConstant.ENABLE);
+
+        //设置记录创建时间
+        employee.setCreateTime(LocalDateTime.now());
+
+        //设置最后更新时间
+        employee.setUpdateTime(LocalDateTime.now());
+
+
+        //创建人id
+        employee.setCreateUser(BaseContext.getCurrentId());
+
+        //最后更新人id
+        employee.setUpdateUser(BaseContext.getCurrentId());
+
+
+        employeeMapper.insert(employee);
+    }
+
+
 
 }
